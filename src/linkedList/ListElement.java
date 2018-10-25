0425da6bec848cbcc99a91c93b3df8fa3bac627e
RayK3 <rdk3@live.ca>
2018-10-25 17:56:58 -0600

    Created a list element and set its value to 5

--- src/linkedList/ListElement.java
+++ src/linkedList/ListElement.java
@@ -8,5 +8,20 @@ package linkedList;
  *
  */
 public class ListElement {
-
+	private ListElement next;
+	private int data;
+	
+	public ListElement()
+	{
+		this.data = 0;
+		this.next = null;
+	}
+	
+	public void setData(int data) {
+		this.data = data;
+	}
+	
+	public int getData() {
+		return this.data;
+	}
 }
--- src/linkedList/UI.java
+++ src/linkedList/UI.java
@@ -14,7 +14,9 @@ public class UI {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ListElement le = new ListElement();
+		le.setData(5);
+		System.out.print(le.getData());
 	}
 
 }
