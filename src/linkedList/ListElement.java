bbc9e9b3c1e92d8b48431fae0c6aad616563d981
RayK3 <rdk3@live.ca>
2018-10-25 17:16:19 -0600

    "First commit"

---
+++ src/linkedList/ListElement.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package linkedList;
+
+/**
+ * @author RDK3
+ *
+ */
+public class ListElement {
+
+}
---
+++ src/linkedList/UI.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package linkedList;
+
+/**
+ * @author RDK3
+ *
+ */
+public class UI {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
